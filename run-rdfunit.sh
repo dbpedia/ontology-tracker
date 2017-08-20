244abdad8ac9dae942dbf966b487f317ceceb321
Gustavo Publio <gpublio@hotmail.com>
2017-08-20 17:10:28 +0200

    updated travis configuration and bash script

--- run-rdfunit.sh
+++ run-rdfunit.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-(cd ./RDFUnit && ./bin/rdfunit -A -d ../../ontology/dbpedia_2016-10.owl -s dbo -r extended)
-exit 0
+OUTPUT="$(cd ./RDFUnit && ./bin/rdfunit -A -d https://rawgit.com/gcpdev/ontology-tracker/master/ontology/dbpedia_2016-10.owl -s dbo -r extended 2>&1)"
+FAIL=$(echo "$OUTPUT" | grep "Tests run" | cut -d ',' -f2 | tr -dc '0-9')
+echo $FAIL;
+if [ $FAIL = 0 ]; then
+  exit 0
+else
+  exit 1
+fi

b1095a3519b24a1ff519fd355e523b23bfe53272
Gustavo Publio <gpublio@hotmail.com>
2017-08-20 17:14:34 +0200

    updated bash script

--- run-rdfunit.sh
+++ run-rdfunit.sh
@@ -2,9 +2,11 @@
 
 OUTPUT="$(cd ./RDFUnit && ./bin/rdfunit -A -d https://rawgit.com/gcpdev/ontology-tracker/master/ontology/dbpedia_2016-10.owl -s dbo -r extended 2>&1)"
 FAIL=$(echo "$OUTPUT" | grep "Tests run" | cut -d ',' -f2 | tr -dc '0-9')
-echo $FAIL;
+echo "Tests failed: $FAIL";
 if [ $FAIL = 0 ]; then
+  echo "Build succeed!"
   exit 0
 else
+  echo "Build failed."
   exit 1
 fi

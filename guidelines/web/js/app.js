adf8e821cc9ccd423736c9559a2bc5a6151062e9
Gustavo Publio <gpublio@hotmail.com>
2017-11-09 23:54:13 +0100

    initial frontend prototype

--- guidelines/web/js/app.js
+++ guidelines/web/js/app.js
@@ -34,3 +34,55 @@ function nextTab(elem) {
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
+
++ function($) {
+    'use strict';
+
+    // UPLOAD CLASS DEFINITION
+    // ======================
+
+    var dropZone = document.getElementById('drop-zone');
+    var uploadForm = document.getElementById('js-upload-form');
+
+    var startUpload = function(files) {
+        console.log(files)
+    }
+
+    uploadForm.addEventListener('submit', function(e) {
+        var uploadFiles = document.getElementById('js-upload-files').files;
+        e.preventDefault()
+
+        startUpload(uploadFiles)
+    })
+
+    dropZone.ondrop = function(e) {
+        e.preventDefault();
+        this.className = 'upload-drop-zone';
+
+        startUpload(e.dataTransfer.files)
+    }
+
+    dropZone.ondragover = function() {
+        this.className = 'upload-drop-zone drop';
+        return false;
+    }
+
+    dropZone.ondragleave = function() {
+        this.className = 'upload-drop-zone';
+        return false;
+    }
+
+}(jQuery);
+
+jQuery(function($) {
+    var loadedForm;
+    $.getJSON("../guideline_form.json", function(loadedForm) {
+        console.log(loadedForm); // this will show the info it in firebug console
+        var fbTemplate = document.getElementById('fb-template');
+        $('.fb-render').formRender({
+          dataType: 'json',
+          formData: loadedForm
+        });
+    });    
+    
+  });
